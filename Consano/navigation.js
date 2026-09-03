1fbafc66db86b30255a98310d7e515d29efdf3bd
sabsterdreams <sabinapmadden@gmail.com>
2026-09-03 21:01:14 +0100

    Change nav links display style from block to flex

--- Consano/navigation.js
+++ Consano/navigation.js
@@ -2,12 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 let menuOpen = false;
 
-hamburger.addEventListener('click',() => {
-  if (menuOpen ==false){
-    navLinks.style.display = "block";
+hamburger.addEventListener('click', () => {
+  if (menuOpen === false) {
+    navLinks.style.display = "flex";
     menuOpen = true;
-  }
-  else if (menuOpen == true) {
+  } else {
     navLinks.style.display = "none";
     menuOpen = false;
   }
